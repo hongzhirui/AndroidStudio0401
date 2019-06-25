68d1de46d764b3537a0e85cd05bf09039dd9e910
hongzhirui <1162996795@qq.com>
2019-06-25 19:58:16 +0800

    百度外卖支付界面

---
+++ app/src/main/java/example/androidstudio0401/MainActivity.java
@@ -0,0 +1,17 @@
+package example.androidstudio0401;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
